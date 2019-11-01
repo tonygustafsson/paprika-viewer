31a4aa8738d9e32fee114a89934a8bb3a8f4e536
Tony Gustafsson <mail@tonyg.se>
2019-11-01 16:15:02 +0100

    Show filter if no coins is showing

--- src/App.svelte
+++ src/App.svelte
@@ -45,55 +45,57 @@
         </p>
     {/if}
 
-    {#if !$global.loading && $tickers.length > 0}
+    {#if !$global.loading}
         <Filter />
 
-        <table>
-            <tr>
-                <th on:click={() => sort('rank')}>#</th>
-                <th on:click={() => sort('symbol')}>Symbol</th>
-                <th on:click={() => sort('name')}>Name</th>
-                <th on:click={() => sort('price')}>Price</th>
-                <th on:click={() => sort('marketcap')}>Marketcap</th>
-                <th on:click={() => sort('volume_24h')}>Volume 24h</th>
-                <th on:click={() => sort('1h')}>1h</th>
-                <th on:click={() => sort('12h')}>12h</th>
-                <th on:click={() => sort('24h')}>24h</th>
-                <th on:click={() => sort('7d')}>7d</th>
-                <th on:click={() => sort('30d')}>30d</th>
-                <th on:click={() => sort('ath')}>From ATH</th>
-                <th>Exchanges</th>
-            </tr>
-
-            {#each $tickers as ticker}
+        {#if $tickers.length > 0}
+            <table>
                 <tr>
-                    <td>{ticker.rank}</td>
-                    <td>{ticker.symbol}</td>
-                    <td>{ticker.name}</td>
-                    <td>{ticker.quotes.USD.price.toFixed(2)} USD</td>
-                    <td>{ticker.quotes.USD.market_cap} USD</td>
-                    <td>{ticker.quotes.USD.volume_24h.toFixed(0)} USD</td>
-                    <td>{ticker.quotes.USD.percent_change_1h}%</td>
-                    <td>{ticker.quotes.USD.percent_change_12h}%</td>
-                    <td>{ticker.quotes.USD.percent_change_24h}%</td>
-                    <td>{ticker.quotes.USD.percent_change_7d}%</td>
-                    <td>{ticker.quotes.USD.percent_change_30d}%</td>
-                    <td>{ticker.quotes.USD.percent_from_price_ath}%</td>
-                    <td class="exchanges">
-                        {#if ticker.exchanges}
-                            <ul>
-                                {#each ticker.exchanges as exchange}
-                                    <li>{exchange}</li>
-                                {/each}
-                            </ul>
-                        {/if}
-                    </td>
+                    <th on:click={() => sort('rank')}>#</th>
+                    <th on:click={() => sort('symbol')}>Symbol</th>
+                    <th on:click={() => sort('name')}>Name</th>
+                    <th on:click={() => sort('price')}>Price</th>
+                    <th on:click={() => sort('marketcap')}>Marketcap</th>
+                    <th on:click={() => sort('volume_24h')}>Volume 24h</th>
+                    <th on:click={() => sort('1h')}>1h</th>
+                    <th on:click={() => sort('12h')}>12h</th>
+                    <th on:click={() => sort('24h')}>24h</th>
+                    <th on:click={() => sort('7d')}>7d</th>
+                    <th on:click={() => sort('30d')}>30d</th>
+                    <th on:click={() => sort('ath')}>From ATH</th>
+                    <th>Exchanges</th>
                 </tr>
-            {/each}
-        </table>
-    {:else if !$global.loading}
-        <p>
-            <em>No matching coins...</em>
-        </p>
+
+                {#each $tickers as ticker}
+                    <tr>
+                        <td>{ticker.rank}</td>
+                        <td>{ticker.symbol}</td>
+                        <td>{ticker.name}</td>
+                        <td>{ticker.quotes.USD.price.toFixed(2)} USD</td>
+                        <td>{ticker.quotes.USD.market_cap} USD</td>
+                        <td>{ticker.quotes.USD.volume_24h.toFixed(0)} USD</td>
+                        <td>{ticker.quotes.USD.percent_change_1h}%</td>
+                        <td>{ticker.quotes.USD.percent_change_12h}%</td>
+                        <td>{ticker.quotes.USD.percent_change_24h}%</td>
+                        <td>{ticker.quotes.USD.percent_change_7d}%</td>
+                        <td>{ticker.quotes.USD.percent_change_30d}%</td>
+                        <td>{ticker.quotes.USD.percent_from_price_ath}%</td>
+                        <td class="exchanges">
+                            {#if ticker.exchanges}
+                                <ul>
+                                    {#each ticker.exchanges as exchange}
+                                        <li>{exchange}</li>
+                                    {/each}
+                                </ul>
+                            {/if}
+                        </td>
+                    </tr>
+                {/each}
+            </table>
+        {:else}
+            <p>
+                <em>No matching coins...</em>
+            </p>
+        {/if}
     {/if}
 </div>
