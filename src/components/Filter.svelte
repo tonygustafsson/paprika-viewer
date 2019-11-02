df3ac4f532d6854a18d8c0f86e0f8fae04983fb9
Tony Gustafsson <mail@tonyg.se>
2019-11-02 14:19:18 +0100

    Some style for exchange filter and more exchanges

--- src/components/Filter.svelte
+++ src/components/Filter.svelte
@@ -11,14 +11,24 @@
     .filter {
         padding-bottom: 1em;
     }
+
+    select {
+        padding: 0.2em;
+        margin: 1em;
+        font-size: 100%;
+    }
 </style>
 
 <div class="filter">
-    <label for="filter-exchange">Exchange</label>
+    <label for="filter-exchange">Exchange:</label>
     <select id="filter-exchange" name="filter-exchange" on:change={e => filterExchange(e)}>
         <option value="all">All</option>
-        <option value="coinbase">Coinbase</option>
         <option value="binance">Binance</option>
+        <option value="coinbase">Coinbase</option>
+        <option value="idax">IDAX</option>
         <option value="idex">IDEX</option>
+        <option value="kraken">Kraken</option>
+        <option value="kucoin">Kucoin</option>
+        <option value="okex">OKEx</option>
     </select>
 </div>
