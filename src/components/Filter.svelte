c7ac7f588a4450f8d34f64f5ce170be0ad0cffce
Tony Gustafsson <mail@tonyg.se>
2019-11-19 16:30:58 +0100

    Mobile fixes for filters

--- src/components/Filter.svelte
+++ src/components/Filter.svelte
@@ -24,9 +24,21 @@
 </script>
 
 <style>
+    .filters {
+        display: inline-flex;
+        flex-wrap: wrap;
+    }
+
+    .filter-item {
+        flex: 1 0 0;
+        align-items: center;
+        display: flex;
+        margin: 0 12px;
+    }
+
     select {
         padding: 0.2em 0.5em;
-        margin: 1em 0.5em;
+        margin: 0.2em 0.5em;
         background-color: #202020;
         color: #fff;
         font-size: 100%;
@@ -34,8 +46,13 @@
         vertical-align: middle;
     }
 
+    label {
+        min-width: 5em;
+        text-align: right;
+    }
+
     input[type='checkbox'] {
-        margin: 1em 0.5em;
+        margin: 0.2em 0.5em;
         background-color: #202020;
         color: #fff;
         font-size: 100%;
@@ -54,55 +71,67 @@
     }
 </style>
 
-<div class="filter">
-    <label for="filter-exchange">Exchange:</label>
-    <select id="filter-exchange" name="filter-exchange" value={$filter.exchange} on:change={e => filterExchange(e)}>
-        <option value="all">All</option>
-        <option value="any">Any</option>
-        <option value="binance">Binance</option>
-        <option value="coinbase">Coinbase</option>
-        <option value="idax">IDAX</option>
-        <option value="idex">IDEX</option>
-        <option value="kraken">Kraken</option>
-        <option value="kucoin">Kucoin</option>
-        <option value="okex">OKEx</option>
-    </select>
+<div class="filters">
+    <div class="filter-item">
+        <label for="filter-exchange">Exchange:</label>
+        <select id="filter-exchange" name="filter-exchange" value={$filter.exchange} on:change={e => filterExchange(e)}>
+            <option value="all">All</option>
+            <option value="any">Any</option>
+            <option value="binance">Binance</option>
+            <option value="coinbase">Coinbase</option>
+            <option value="idax">IDAX</option>
+            <option value="idex">IDEX</option>
+            <option value="kraken">Kraken</option>
+            <option value="kucoin">Kucoin</option>
+            <option value="okex">OKEx</option>
+        </select>
+    </div>
 
-    <label for="filter-marketcap">MarketCap:</label>
-    <select id="filter-marketcap" name="filter-marketcap" value={$filter.marketCap} on:change={e => filterMarketCap(e)}>
-        <option value="all">All</option>
-        <option value="0">0 - 100k</option>
-        <option value="100000">100k - 250k</option>
-        <option value="250000">250k - 500k</option>
-        <option value="500000">500k - 1m</option>
-        <option value="1000000">1m - 5m</option>
-        <option value="5000000">5m - 10m</option>
-        <option value="10000000">10m - 20m</option>
-        <option value="20000000">20m - 50m</option>
-        <option value="50000000">50m+</option>
-    </select>
+    <div class="filter-item">
+        <label for="filter-marketcap">MarketCap:</label>
+        <select
+            id="filter-marketcap"
+            name="filter-marketcap"
+            value={$filter.marketCap}
+            on:change={e => filterMarketCap(e)}>
+            <option value="all">All</option>
+            <option value="0">0 - 100k</option>
+            <option value="100000">100k - 250k</option>
+            <option value="250000">250k - 500k</option>
+            <option value="500000">500k - 1m</option>
+            <option value="1000000">1m - 5m</option>
+            <option value="5000000">5m - 10m</option>
+            <option value="10000000">10m - 20m</option>
+            <option value="20000000">20m - 50m</option>
+            <option value="50000000">50m+</option>
+        </select>
+    </div>
 
-    <label for="filter-volume">Volume:</label>
-    <select id="filter-volume" name="filter-volume" value={$filter.volume} on:change={e => filterVolume(e)}>
-        <option value="all">All</option>
-        <option value="0">0 - 100k</option>
-        <option value="100000">100k - 250k</option>
-        <option value="250000">250k - 500k</option>
-        <option value="500000">500k - 1m</option>
-        <option value="1000000">1m - 5m</option>
-        <option value="5000000">5m - 10m</option>
-        <option value="10000000">10m - 20m</option>
-        <option value="20000000">20m - 50m</option>
-        <option value="50000000">50m+</option>
-    </select>
+    <div class="filter-item">
+        <label for="filter-volume">Volume:</label>
+        <select id="filter-volume" name="filter-volume" value={$filter.volume} on:change={e => filterVolume(e)}>
+            <option value="all">All</option>
+            <option value="0">0 - 100k</option>
+            <option value="100000">100k - 250k</option>
+            <option value="250000">250k - 500k</option>
+            <option value="500000">500k - 1m</option>
+            <option value="1000000">1m - 5m</option>
+            <option value="5000000">5m - 10m</option>
+            <option value="10000000">10m - 20m</option>
+            <option value="20000000">20m - 50m</option>
+            <option value="50000000">50m+</option>
+        </select>
+    </div>
 
-    <label for="filter-favorites">Favorites only:</label>
-    <input
-        type="checkbox"
-        id="filter-favorites"
-        name="filter-favorites"
-        checked={$filter.favorites}
-        on:change={e => filterFavorites(e)} />
+    <div class="filter-item">
+        <label for="filter-favorites">Favorites only:</label>
+        <input
+            type="checkbox"
+            id="filter-favorites"
+            name="filter-favorites"
+            checked={$filter.favorites}
+            on:change={e => filterFavorites(e)} />
+    </div>
 
     <button on:click={resetFilters}>Reset</button>
 </div>
