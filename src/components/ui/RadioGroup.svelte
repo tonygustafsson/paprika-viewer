0cfeb8e5e04fd14f31c94ae6c705811cd3ac46d3
tony.gustafsson <tony.gustafsson@starrepublic.com>
2022-07-14 23:09:49 +0200

    RadioGroup support label

--- src/components/ui/RadioGroup.svelte
+++ src/components/ui/RadioGroup.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	export let name: string;
+	export let label: string;
 	export let items: {
 		label: string;
 		value: string;
 		checked?: boolean;
 	}[];
 </script>
+
+{#if label}
+	<p>{label}</p>
+{/if}
 
 <div class="wrapper">
 	{#each items as item}
