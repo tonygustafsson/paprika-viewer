1501a3a161062a67cf1b3bcda472597c5546d2ab
tony.gustafsson <tony.gustafsson@starrepublic.com>
2022-07-14 23:09:30 +0200

    Button support size, variant and icon

--- src/components/ui/Button.svelte
+++ src/components/ui/Button.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+	export let variant: 'primary' | 'secondary' = 'secondary';
+	export let size: 'small' | 'medium' = 'small';
 </script>
 
-<button on:click><slot /></button>
+<button on:click class:medium={size === 'medium'} class:primary={variant === 'primary'}>
+	<div class="icon">
+		<slot name="icon" />
+	</div>
+
+	<slot />
+</button>
 
 <style>
 	button {
+		display: flex;
+		align-items: center;
 		font-family: var(--font-family);
 		padding: 0.5em 1em;
 		margin: 1em 0.5em;
@@ -17,7 +27,27 @@
 		transition: background-color 150ms ease-in-out;
 	}
 
+	button.primary {
+		background-color: var(--color-red-500);
+	}
+
+	button.medium {
+		font-size: 1rem;
+		padding: 0.75em 1.5em;
+	}
+
 	button:hover {
 		background-color: var(--color-secondary-hover);
+	}
+
+	button.primary:hover {
+		background-color: var(--color-red-600);
+	}
+
+	.icon {
+		display: flex;
+		color: white;
+		margin-right: 0.25em;
+		vertical-align: middle;
 	}
 </style>
