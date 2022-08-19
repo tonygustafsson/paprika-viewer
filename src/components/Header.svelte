48349f6eb433298ec1cc08e9e421ab8038c64d09
tony.gustafsson <tony.gustafsson@starrepublic.com>
2022-08-19 10:37:41 +0200

    Add nav

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,35 +1,63 @@
 <script lang="ts">
 	import GlobalMarket from '../components/GlobalMarket.svelte';
 	import ChiliIcon from '../components/icons/Chili.svelte';
+	import { page } from '$app/stores';
 
 	export let isStartPage: boolean | undefined = undefined;
 </script>
 
 <GlobalMarket />
 
-{#if isStartPage}
-	<h1>
-		<a href="/">
-			<ChiliIcon width={35} height={35} /> Paprika Viewer
-		</a>
-	</h1>
-{/if}
+<div class="wrapper">
+	{#if isStartPage}
+		<h1>
+			<a href="/" class="logo">
+				<ChiliIcon width={35} height={35} /> Paprika Viewer
+			</a>
+		</h1>
+	{/if}
 
-{#if !isStartPage}
-	<span class="logo">
-		<a href="/">
+	{#if !isStartPage}
+		<a href="/" class="logo">
 			<ChiliIcon width={35} height={35} /> Paprika Viewer
 		</a>
-	</span>
-{/if}
+	{/if}
+
+	<nav>
+		<ul>
+			<li>
+				<a href="/" class:active={$page.url.pathname === '/'}>Tickers</a>
+			</li>
+			<li>
+				<a href="/news" class:active={$page.url.pathname === '/news'}>News</a>
+			</li>
+		</ul>
+	</nav>
+</div>
 
 <style>
-	h1 {
+	.wrapper {
 		display: flex;
 		align-items: flex-end;
-		gap: 8px;
-		font-size: 1.75rem;
-		margin-block: 0.75em;
+		gap: 48px;
+		margin-block: 48px;
+	}
+
+	nav ul {
+		display: flex;
+		gap: 24px;
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	nav ul li {
+		display: inline-block;
+		font-size: 1.25rem;
+	}
+
+	nav ul li a.active {
+		text-decoration: underline;
 	}
 
 	a {
@@ -37,11 +65,14 @@
 		text-decoration: none;
 	}
 
+	h1 {
+		margin: 0;
+	}
+
 	.logo {
 		display: flex;
 		align-items: flex-end;
 		gap: 8px;
-		margin-block: 0.75em;
 		font-size: 1.75rem;
 		font-weight: bold;
 	}
